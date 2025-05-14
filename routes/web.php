b4e1e5f4840e83e9e6a48ff231ee290142a3d7d0
Diva <dvnsputri@gmail.com>
2025-05-14 09:21:08 +0700

    pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+// use di php sama kayak import di java
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Coba;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// kurleb sama kayak System.out.println()
+// di java kan pake . nah kalo di php pake nya ::
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/selamat', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello', [Coba:: class, 'helloworld']);
