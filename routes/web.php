bd77464dd38cc94b4c88f78b3a94d76e2b292749
Diva <dvnsputri@gmail.com>
2025-06-03 15:36:01 +0700

    CRUD LENGKAP

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Coba;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 /*
 |--------------------------------------------------------------------------
@@ -101,7 +102,7 @@ Route::get('frontend', function () {
 
 
 Route::get('dosen', [Coba:: class, 'index']);
-Route::get('/pegawai/{nama}', [PegawaiController:: class, 'index']);
+// Route::get('/pegawai/{nama}', [PegawaiController:: class, 'index']);
 
 Route::get('/formulir', [PegawaiController:: class, 'formulir']); //halaman isian formulir
 Route::post('/formulir/proses', [PegawaiController:: class, 'proses']); //action form
@@ -110,3 +111,15 @@ Route::post('/formulir/proses', [PegawaiController:: class, 'proses']); //action
 Route::get('/blog', [BlogController:: class, 'home']);
 Route::get('/blog/tentang', [BlogController:: class, 'tentang']);
 Route::get('/blog/kontak', [BlogController:: class, 'kontak']);
+
+
+// CRUD
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController:: class, 'store']);
+
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController:: class, 'update']);
+
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController:: class, 'hapus']);
